6106806becf9b8efb05afe9768baeedb8e20d9d5
sulheebaek <39540878+sulheebaek@users.noreply.github.com>
2022-07-11 17:25:28 +0900

    Day1

---
+++ 강사노트북/forWhileTest.py
@@ -0,0 +1,16 @@
+# for 루프 테스트
+# 1부터 10까지 합계 구하기
+total1 = 0.0
+
+for i in range(1, 11):
+    total1 = total1 + i
+print(total1)
+
+# while 루프 이용
+
+total2 = 0.0
+i = 1
+while i < 11:
+    total2 = total2 +i
+    i = i + 1
+print(total2)
